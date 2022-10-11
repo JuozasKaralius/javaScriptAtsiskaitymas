277d78a75226a06cd00b2df82fb9d77d0df75b7d
juozas <juozas.karalius@codeacademylt.onmicrosof.com>
2022-10-11 20:25:26 +0300

    atlikta pirma uzduotis, sukurtas konverteris. CSS  truputi pastilizuota output.

---
+++ JS Tasks/Task 1/script.js
@@ -0,0 +1,36 @@
+/* ------------------------------ TASK 1 ----------------------------
+Parašykite JS kodą, kuris leis vartotojui įvesti svorį kilogramais ir
+pamatyti jo pateikto svorio kovertavimą į:
+1. Svarus (lb) | Formulė: lb = kg * 2.2046
+2. Gramus (g) | Formulė: g = kg / 0.0010000
+3. Uncijos (oz) | Formul4: oz = kg * 35.274
+
+Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
+<div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
+------------------------------------------------------------------- */
+
+    
+const convert = document.getElementById('submit-btn');
+convert.addEventListener('click',kiloweightConverter);
+
+convert.onclick = function() {
+
+return false;
+}
+
+
+
+function kiloweightConverter() {
+    const kilograms = document.getElementById('search').value;
+    document.getElementById('output').style.visibility = 'visible'
+
+    document.getElementById('svarai').innerHTML = kilograms * 2.2046;
+    document.getElementById('gramai').innerHTML = kilograms / 0.0010000;
+    document.getElementById('uncijos').innerHTML = kilograms * 35.274;
+
+   
+}
+
+
+
+
