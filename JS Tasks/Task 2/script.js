81cfbc4cd21bac2024b623847c040e3db58cc414
juozas <juozas.karalius@codeacademylt.onmicrosof.com>
2022-10-11 21:01:54 +0300

    javaScript padaryta funkcija kuri skaiciuoja mygtuko paspaudimus ir css uzdeta truputi stiliaus

--- JS Tasks/Task 2/script.js
+++ JS Tasks/Task 2/script.js
@@ -3,3 +3,11 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+
+function Calculator() {
+const clickCalculator = document.getElementById('btn__element');
+clickCalculator.addEventListener('click', e => document.getElementById('btn__state')
+.innerHTML = ++ document.getElementById ('btn__state').innerHTML);
+}
+
+Calculator();
